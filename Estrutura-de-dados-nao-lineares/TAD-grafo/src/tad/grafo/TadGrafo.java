9d43311c6065fecd6611b0e1d8e33b4e4d943dbb
rutefernandes <rutefdn@hotmail.com>
2019-10-22 08:11:04 -0300

    adicionados casos de teste

--- Estrutura-de-dados-nao-lineares/TAD-grafo/src/tad/grafo/TadGrafo.java
+++ Estrutura-de-dados-nao-lineares/TAD-grafo/src/tad/grafo/TadGrafo.java
@@ -1,5 +1,7 @@
 package tad.grafo;
 
+import java.util.Vector;
+
 /**
  *
  * @author RUTE
@@ -10,7 +12,24 @@ public class TadGrafo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	 Grafo g = new Grafo();
+         Vertice v1 = new Vertice(1, 20);
+         Vertice v2 = new Vertice(2, 20);
+         Vertice v3 = new Vertice(3, 20);
+         Vertice v4 = new Vertice(4, 20);
+         g.inserirVertice(v1);
+         g.inserirVertice(v2);
+         g.inserirVertice(v3);
+         g.inserirVertice(v4);
+         g.insereAresta(v1, v2, 1);
+         g.insereAresta(v1, v3, 2);
+     //    g.mostraMatriz();
+         System.out.println(g.grau(v4));
+         Vector<Aresta> arestas = g.ArestaIncidentes(v1);
+         for(int i = 0; i < arestas.size(); i++) {
+             System.out.println(arestas.get(i));
+         }
+     }
     }
-    
-}
+   
+
